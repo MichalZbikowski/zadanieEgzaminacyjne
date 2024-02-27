71ad7fa9dc8801423187d67f4773e5f2bef4f883
Michał Żbikowski <152189727+MichalZbikowski@users.noreply.github.com>
2024-02-27 13:04:01 +0100

    Add files via upload

--- PESEL/dodanie.php
+++ PESEL/dodanie.php
@@ -8,7 +8,7 @@ $imie2 = $_POST['imie2'];
 $imie3 = $_POST['imie3'];
 
 
-$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES (20, '$imie1', '$imie2', '$imie3')";
+$sql = "INSERT INTO `ratownicy`(`nrKaretki`, `ratownik1`, `ratownik2`, `ratownik3`) VALUES ($nrKaretki, '$imie1', '$imie2', '$imie3')";
 $zapytanie = mysqli_query($conn,$sql);
 echo "Do bazy zostało wysłane zapytanie: ".$sql;
 mysqli_close($conn);
